0b5901f0d9ab2d6a5c3270d12cabc956f47f1324
Clinton Kline <73147235+ClntK@users.noreply.github.com>
2022-09-16 20:45:22 -0400

    Add files via upload

---
+++ DOCX to PDF Converter.ps1
@@ -0,0 +1,49 @@
+
+function docxtopdf() {
+    #Browsing file
+
+    Write-host(">> Choose .docx file to convert: ")
+    Start-Sleep(1)
+
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+    # titled 'Choose a DOCX to convert"
+    $FileBrowser.title = "Choose a DOCX to convert"
+    # that filters docx files.
+    $FileBrowser.filter = "docx (*.docx)| *.docx"
+    # open file dialog
+    [void]$FileBrowser.ShowDialog()
+    # assign selected file to a variable
+    $InputFile = $FileBrowser.FileName
+
+    write-host("`n>> `"" + $InputFile + "`" is being converted to PDF format...")
+
+    # create a word object
+    $Word = NEW-OBJECT -COMOBJECT WORD.APPLICATION
+    # get the current permissions for the docx file
+    $ACL = Get-ACL -Path $InputFile
+    # get the current username
+    $user = $env:UserName
+    # set the permissions of the docx file to 'full control'
+    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($user,"FullControl","Allow")
+    $ACL | Set-Acl -Path $InputFile
+    Write-Host("`n>> Full control permissions granted to file...`n")
+    # $ACL = Get-ACL -Path $InputFile
+
+    # open a Word document, filename from the directory
+    $Doc=$Word.Documents.Open($InputFile)
+
+    # Swap out DOCX with PDF in the Filename
+    $Name=$Doc.Fullname.replace("docx","pdf")
+
+    # Save this File as a PDF in Word 2010/2013
+    $Doc.saveas([ref] $Name, [ref] 17)  
+
+    Write-Host("`n>> DOCX has been converted.`n")
+    Start-Sleep(1)
+    
+    $Doc.close()
+    exit    
+}
+
+docxtopdf
